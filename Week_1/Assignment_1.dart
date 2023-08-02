070ef34ad2aef27ec27cc0ba3a1d599573481e90
Jubayer Ahmed <skjubayer21@gmail.com>
2023-08-02 22:20:59 +0600

    Week 1: Assignment 1 code

---
+++ Week_1/Assignment_1.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  int a = 7;
+  int b = 5;
+
+  int addition = a + b;
+  int subtraction = a - b;
+  int multiplication = a * b;
+  double division = a / b;
+  int modulus = a % b;
+
+  print('Addition: $addition');
+  print('Subtraction: $subtraction');
+  print('Multiplication: $multiplication');
+  print('Division: $division');
+  print('Modulus: $modulus');
+
+}
